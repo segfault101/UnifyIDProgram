c1945e5f37eae6684a1969577fc35016d64f8dc6
Rahul <vemurirahul93@gmail.com>
2017-05-15 21:16:38 -0400

    cleaning up imports and comments.

--- random_image_generator.py
+++ random_image_generator.py
@@ -1,11 +1,5 @@
 from PIL import Image
-from random import randint
 import urllib2
-
-"""
-def get_random():
-	return randint(0, 255)
-"""
 
 def get_random():
 	request = urllib2.Request('http://www.random.org/integers/?num=1&min=0&max=255&col=1&base=10&format=plain&rnd=new')
